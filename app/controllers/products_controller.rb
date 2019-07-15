99aff904f53fd11e52a6bd13b98e4cb88a43cecf
Erfandi Suryo Putra <13515145@std.stei.itb.ac.id>
2019-07-16 05:41:26 +0700

    Add error response

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,30 +1,36 @@
 class ProductsController < ApplicationController
   def index
     product = Product.order('id')
-    render json: {code: 200, msg: "success", data: product}, status: :ok
+    render json: {code: 200, msg: "Success", data: product}, status: :ok
   end
 
   def show
     product = Product.find(params[:id])
-    render json: {code: 200, msg: "success", data: product}, status: :ok
+    render json: {code: 200, msg: "Success", data: product}, status: :ok
   end
 
   def create
     product = Product.new(product_params)
-    product.save
-    render json: {code: 200, msg: "success", data: product}, status: :ok
+    if product.save
+      render json: {code: 200, msg: "Success", data: product}, status: :ok
+    else
+      render json: {code: 400, msg: "Error"}, status: :unprocessable_entity
+    end
   end
 
   def destroy
     product = Product.find(params[:id])
     product.destroy
-    render json: {code: 200, msg: "success", data: []}, status: :ok
+    render json: {code: 200, msg: "Success", data: []}, status: :ok
   end
 
   def update
     product = Product.find(params[:id])
-    product.update_attributes(product_params)
-    render json: {code: 200, msg: "success", data: product}, status: :ok
+    if product.update_attributes(product_params)
+      render json: {code: 200, msg: "Success", data: product}, status: :ok
+    else
+      render json: {code: 400, msg: "Error"}, status: :unprocessable_entity
+    end
   end
 
   private
